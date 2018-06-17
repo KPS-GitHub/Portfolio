1fc101b44e3ecdd0b66bcbdf3877812af0f4f5f7
Kennan Smith <kennan.smith1@gmail.com>
2018-06-17 13:23:23 -0600

    added basic html for homepage in homeHtml variable

--- logic.js
+++ logic.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
 
 // html "pages"
-var homeHtml = "";
-var portfolioHtml = "";
-var contactMeHtml = "";
+var homeHtml = "" + 
+
+                "<div id='container'>" + 
+                    "<div class='homeTitle row text-center'>" +
+                        "<h2>Kennan Smith - Full-Stack Web Developer, Lover of Creativity</h2>" +
+                    "</div>" +
+                    "<div class='homePic row text-center'>" +
+                        "<img src='assets/images/suited_headshot.jpg' alt='Headshot of Kennan Smith' />"
+                    "</div>" +
+                    "<div class='homeSummary row text-center'>" +
+                        "<h4>A Passion for Simple, yet Unique Creations</h4>" +
+                        "<p>I love simple beauty and I am also aware that oversimplifying leads to a boring product that has no way of distinguishing itself from the crowd. This is why I go into every Web Development project with the goals of a seamless user experience, a simple presentation, and a creative flair that provides the project with its own personality.</p>" +
+                    "</div>" +
+                    "<div class='row'>" +
+                        "<button id='portfolioButton'>My Projects</button>" +
+                    "</div>" +
+                "</div>" +
+
+                "";
+
+var portfolioHtml = "" +
+
+
+
+                    "";
+
+var contactMeHtml = "" +
+
+
+
+                    "";
 
 // jquery that displays the correct html "page"
 
