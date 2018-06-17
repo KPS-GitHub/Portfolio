35b6dfd5713df373315ee23371aad088aa401a10
Kennan Smith <kennan.smith1@gmail.com>
2018-06-17 13:40:20 -0600

    still debugging the fact that the homeHtml is not displaying - removed the empty strings at beginning and end of homeHtml - if that doesn't work, I will try to rework my jquery, possibly into a function

--- logic.js
+++ logic.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
 
 // html "pages"
-var homeHtml = "" + 
-
-                    "<div class='homeTitle row text-center'>" +
-                        "<h2>Kennan Smith - Full-Stack Web Developer, Lover of Creativity</h2>" +
-                    "</div>" +
-                    "<div class='homePic row text-center'>" +
-                        "<img src='assets/images/suited_headshot.jpg' alt='Headshot of Kennan Smith' />" +
-                    "</div>" +
-                    "<div class='homeSummary row text-center'>" +
-                        "<h4>A Passion for Simple, yet Unique Creations</h4>" +
-                        "<p>I love simple beauty and I am also aware that oversimplifying leads to a boring product that has no way of distinguishing itself from the crowd. This is why I go into every Web Development project with the goals of a seamless user experience, a simple presentation, and a creative flair that provides the project with its own personality.</p>" +
-                    "</div>" +
-                    "<div class='row'>" +
-                        "<button id='portfolioButton'>My Projects</button>" +
-                    "</div>" +
-
-                "";
+var homeHtml =  "<div class='homeTitle row text-center'>" +
+                    "<h2>Kennan Smith - Full-Stack Web Developer, Lover of Creativity</h2>" +
+                "</div>" +
+                "<div class='homePic row text-center'>" +
+                    "<img src='assets/images/suited_headshot.jpg' alt='Headshot of Kennan Smith' />" +
+                "</div>" +
+                "<div class='homeSummary row text-center'>" +
+                    "<h4>A Passion for Simple, yet Unique Creations</h4>" +
+                    "<p>I love simple beauty and I am also aware that oversimplifying leads to a boring product that has no way of distinguishing itself from the crowd. This is why I go into every Web Development project with the goals of a seamless user experience, a simple presentation, and a creative flair that provides the project with its own personality.</p>" +
+                "</div>" +
+                "<div class='row'>" +
+                    "<button id='portfolioButton'>My Projects</button>" +
+                "</div>";
 
 var portfolioHtml = "" +
 
