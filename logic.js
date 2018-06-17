e12bea0c220f91abcdb3ef333744c7c02ecdc862
Kennan <kennan.smith1@gmail.com>
2018-06-17 01:16:39 -0600

    added assets and assets/images folders; a few starter lines of code/pseudocode in logic.js; added main content div to home.html

---
+++ logic.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+// html "pages"
+var homeHtml = "";
+var portfolioHtml = "";
+var contactMeHtml = "";
+
+// jquery that displays the correct html "page"
+
+
+}); //end of docready
